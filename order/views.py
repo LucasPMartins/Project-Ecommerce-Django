3f18af4dcc126aed9a64dde96d6ed82ef9da7ee9
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2025-01-14 15:38:06 -0300

    Add migrations for OrderItem and Attribute models; implement product list template and base layout

--- order/views.py
+++ order/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
+from django.http import HttpResponse
 
 class PaymentView(View):
     pass
--- product/views.py
+++ product/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
+from . import models
 
 class ProductListView(ListView):
-    ...
+    model = models.Product
+    template_name = 'product/list.html'
+    context_object_name = 'products'
+
 
 class ProductDetailView(View):
     ...
