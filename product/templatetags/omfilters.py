48a1eb5ea28dcbec6a79a4145f292b50441aee9a
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2025-01-14 16:11:15 -0300

    Refactor price formatting in product list template; move format_price logic to a new utility module

--- product/templatetags/omfilters.py
+++ product/templatetags/omfilters.py
@@ -1,7 +1,8 @@
 from django.template import Library
+from utils import format
 
 register = Library()
 
 @register.filter
 def format_price(value):
-    return f'R$ {value:.2f}'.replace('.', ',')
+    return format.format_price(value)
