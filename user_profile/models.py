713b4c04386ef7bea0d58c0e8123bec1318f5314
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2025-01-17 10:26:23 -0300

    Alter UserProfile CPF field to be unique and update migration

--- user_profile/models.py
+++ user_profile/models.py
@@ -8,7 +8,7 @@ from datetime import datetime
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthday = models.DateField()
-    cpf = models.CharField(max_length=11,verbose_name='CPF')
+    cpf = models.CharField(max_length=11,verbose_name='CPF',unique=True)
     address = models.CharField(max_length=50)
     number = models.CharField(max_length=5)
     complement = models.CharField(max_length=30,blank=True)
